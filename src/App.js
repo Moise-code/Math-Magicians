ebff1c2bc048cd4ce313fcaf6732831750df858f
Moise-code <moisewowe@gmail.com>
2023-02-14 15:26:20 +0200

    adding importing the calculator in app.js file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import logo from './logo.svg';
+/* eslint-disable import/no-cycle */
+// import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder={0} />
+      <Calculator value="A/C" />
+      <Calculator value="+/-" />
+      <Calculator value="%" />
+      <Calculator value="/" />
+      <Calculator value="7" />
+      <Calculator value="8" />
+      <Calculator value="9" />
+      <Calculator value="*" />
+      <Calculator value="4" />
+      <Calculator value="5" />
+      <Calculator value="6" />
+      <Calculator value="-" />
+      <Calculator value="1" />
+      <Calculator value="2" />
+      <Calculator value="3" />
+      <Calculator value="+" />
+      <Calculator value="0" />
+      <Calculator value="." />
     </div>
   );
 }

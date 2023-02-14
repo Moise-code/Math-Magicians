40f19b1ec681df22f0224150af317a165c9511e3
Moise-code <moisewowe@gmail.com>
2023-02-14 17:33:23 +0200

    refactoring the styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 /* eslint-disable import/no-cycle */
 // import React, {Component} from 'react';
 // import logo from './logo.svg';
@@ -7,29 +9,7 @@ import Calculator from './components/Calculator';
 function App() {
   return (
     <div className="App">
-      <input type="text" value={0} />
-      <div className="calcu">
-        <Calculator class="all-clear" value="A/C" />
-        <Calculator value="+/-" />
-        <Calculator value="%" />
-        <Calculator class="operator" value="/" />
-        <Calculator value="7" />
-        <Calculator value="8" />
-        <Calculator value="9" />
-        <Calculator class="operator" value="*" />
-        <Calculator value="4" />
-        <Calculator value="5" />
-        <Calculator value="6" />
-        <Calculator class="operator" value="-" />
-        <Calculator value="1" />
-        <Calculator value="2" />
-        <Calculator value="3" />
-        <Calculator class="operator" value="+" />
-        <Calculator value="0" />
-        <Calculator class="decimal" value="." />
-        <Calculator class="equal-sign" value="=" />
-      </div>
-
+      <Calculator />
     </div>
   );
 }

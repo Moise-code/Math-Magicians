b4dcdd36e83a330aceb564ebb3120c9cbe533ac7
Moise-code <moisewowe@gmail.com>
2023-02-14 18:06:09 +0200

    cleaning boiler plate and fixing linters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-/* eslint-disable import/no-named-as-default-member */
-/* eslint-disable import/no-named-as-default */
-/* eslint-disable import/no-cycle */
-// import React, {Component} from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Calculator from './components/Calculator';
 

92957f11dff620c9ed90ed20c3ceb70daffa4e4c
Moise-code <moisewowe@gmail.com>
2023-02-14 15:23:50 +0200

    adding the directory for calculator file

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/button-has-type */
+
+const Calculator = (props) => (
+  <div className="calcu">
+    <button type="button">{props.value}</button>
+  </div>
+
+);
+export default Calculator;
